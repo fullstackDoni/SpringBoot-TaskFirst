730d6c8b73672017d70fb21d647d4795f49e5703
Doни <daniyar.doskulov@narxoz.kz>
2023-05-13 21:19:41 +0600

    first commit

---
+++ src/main/java/kz/springboot/springtaskfirst/SpringtaskfirstApplication.java
@@ -0,0 +1,13 @@
+package kz.springboot.springtaskfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtaskfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtaskfirstApplication.class, args);
+	}
+
+}
