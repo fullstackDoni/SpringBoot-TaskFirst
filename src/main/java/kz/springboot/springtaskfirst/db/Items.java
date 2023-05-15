5491a2a32d498ea485a027021c3d5159c4a49f5f
Doни <daniyar.doskulov@narxoz.kz>
2023-05-15 19:59:56 +0600

    "test"

---
+++ src/main/java/kz/springboot/springtaskfirst/db/Items.java
@@ -0,0 +1,17 @@
+package kz.springboot.springtaskfirst.db;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+public class Items {
+    Long id;
+    String name;
+    String image;
+    String desc;
+    double price;
+
+}
