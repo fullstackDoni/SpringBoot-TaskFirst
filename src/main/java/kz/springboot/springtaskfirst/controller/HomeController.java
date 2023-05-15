4f972d0f8b8e4921f244026e275655e96e1e40b8
Doни <daniyar.doskulov@narxoz.kz>
2023-05-15 20:47:19 +0600

    "test"

--- src/main/java/kz/springboot/springtaskfirst/controller/HomeController.java
+++ src/main/java/kz/springboot/springtaskfirst/controller/HomeController.java
@@ -1,13 +1,20 @@
 package kz.springboot.springtaskfirst.controller;
 
+import kz.springboot.springtaskfirst.db.DBManager;
+import kz.springboot.springtaskfirst.db.Items;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
 
     @GetMapping(value = "/")
-    public String index(){
+    public String index(Model model){
+        ArrayList<Items> item = DBManager.getItems();
+        model.addAttribute("item",item);
         return "index";
     }
 }
